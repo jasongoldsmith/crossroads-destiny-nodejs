49951c1f53ba7781e0eac08500001f30f92f4932
basant gollapudi <basant.gollapudi@verizon.com>
2016-03-08 17:52:48 -0800

    removed comments

--- app/helpers/push-notification.js
+++ app/helpers/push-notification.js
@@ -17,18 +17,11 @@ PushNotification.init({
   }
 });
 
-utils.l.i("the push notification params =", PushNotification.options);
-//var iosToken = '69937c97ef2386040ea75d8de961a0855b37dc61682b8c5598631781e28eac0f';
-//var androidToken = "dGnjZK7qshg:APA91bF-1GOMkrGYAWJIHhWCN7qTnNQuvI8fY7DCdfAWoHbOdocDv9AUfee0jNljnNM1FY4sB99ag2qfEhNwu8cm1mzTaRiNl3ikbCtoQLbPZm5Huu11wrKS3LIh3b88dySAerEo1Jb2";
-
-
 function sendSinglePushNotification(data, alert, installation) {
   if(utils._.isInvalidOrBlank(installation)) {
     return;
   }
   PushNotification.prepare("test", alert, installation.unReadNotificationCount, sound, data);
-  utils.l.i("the push notification =",installation.deviceType );
-  utils.l.i("the push notification =",installation.deviceToken );
   PushNotification.addTarget(installation.deviceType, installation.deviceToken);
   PushNotification.push();
 }
