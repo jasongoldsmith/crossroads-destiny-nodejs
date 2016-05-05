423631533a47034b96d1fed7cfa36bc17214d886
Sreeharsha Dasa <setty.sreeharshadasaanjayaiah@one.verizon.com>
2016-05-05 11:58:34 -0700

    Config to enable/disable bungie validation

--- app/service/authService.js
+++ app/service/authService.js
@@ -11,17 +11,22 @@ function signupUser(userData, callback) {
 				function(newuser,callback) {
 					//TBD: membershiptype is hardcoded to PSN for now. When we introduce multiple channels change this to take it from userdata
 					// or send notification to both xbox and psn depending on the ID availability
-
-					destinyService.sendBungieMessage(userData.psnId,userData.userName,"PSN",function(error,messageResponse){
-						if(messageResponse){
-							utils.l.d("messageResponse::token==="+messageResponse.token)
-							newuser.psnVerified = "INITIATED"
-							newuser.psnToken = messageResponse.token
-						}else{
-							newuser.psnVerified = "FAILED_INITIATION"
-						}
-						models.user.save(newuser,callback)
-					})
+					if(utils.config.enableBungieIntegration) {
+						console.log("Destiny validation enabled")
+						destinyService.sendBungieMessage(userData.psnId, userData.userName, "PSN", function (error, messageResponse) {
+							if (messageResponse) {
+								utils.l.d("messageResponse::token===" + messageResponse.token)
+								newuser.psnVerified = "INITIATED"
+								newuser.psnToken = messageResponse.token
+							} else {
+								newuser.psnVerified = "FAILED_INITIATION"
+							}
+							models.user.save(newuser, callback)
+						})
+					}else {
+						console.log("Destiny validation disabled")
+						callback(null, newuser)
+					}
 				}
 			],
 			callback
