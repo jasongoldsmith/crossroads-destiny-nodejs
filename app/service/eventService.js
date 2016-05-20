50a4407f9b598929444b17902c4e082b66e94674
Sreeharsha Dasa <setty.sreeharshadasaanjayaiah@one.verizon.com>
2016-05-19 17:15:09 -0700

    Rebase

---
+++ app/service/eventService.js
@@ -0,0 +1,38 @@
+var utils = require('../utils')
+var models = require('../models')
+
+function cleanEventsForPlayer(playerId,callback){
+  utils.async.waterfall([
+    function(callback){
+      models.event.listEventsByUser(playerId,callback)
+    },function(eventList,callback){
+      utils.async.map(eventList,function(event,callback){
+        leaveEvent({eId: event._id,player: playerId},callback)
+      },
+      callback)
+    }
+  ],callback)
+
+}
+
+function leaveEvent(data, callback) {
+  utils.async.waterfall(
+    [
+      function(callback) {
+        models.event.leaveEvent(data, callback)
+      },
+      function(event, callback) {
+        models.user.getById(data.player, function(err, user) {
+          if(utils._.isValidNonBlank(user) && utils._.isValidNonBlank(event)) {
+            service.eventBasedPushNotificationService.sendPushNotificationForLeave(event, user)
+          }
+          callback(null, event)
+        })
+      }
+    ], callback)
+}
+
+module.exports = {
+  leaveEvent:leaveEvent,
+  cleanEventsForPlayer:cleanEventsForPlayer
+}
