9bce50551c6327f549b44b921acd45fa921713ff
Suraj Abichandani <suraj.abichandani@verizon.com>
2016-05-20 11:20:42 -0700

    Cleaning up the job schedulers which were using node-schedule

--- app/service/index.js
+++ app/service/index.js
@@ -2,7 +2,6 @@
 module.exports = {
   reportService: require('./reportService'),
   authService: require('./authService'),
-  eventScheduleService: require('./eventScheduleService'),
   destinyInerface: require('./destinyInterface'),
   eventNotificationTriggerService:require('./eventNotificationTriggerService'),
   eventNotificationService:require('./eventNotificationService'),
