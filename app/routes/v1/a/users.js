66e402a49bdde56ff8ad91acb0d990e82b2d4ee9
Suraj Abichandani <suraj.abichandani@verizon.com>
2016-05-24 16:58:12 -0700

    adding update password API

--- app/routes/v1/a/users.js
+++ app/routes/v1/a/users.js
@@ -4,6 +4,8 @@ var routeUtils = require('./../../routeUtils')
 var models = require('../../../models')
 var utils = require('../../../utils')
 var service = require('../../../service')
+var passwordHash = require('password-hash')
+
 function getSelfUser(req, res) {
   var feedData = {value: req.user}
   routeUtils.handleAPISuccess(req, res, feedData)
@@ -43,12 +45,45 @@ function update(req, res) {
 }
 
 function updateGroup(req, res) {
-  utils.l.i("Update user request" + JSON.stringify(req.body))
-  req.assert('clanId', "clanId is a required field").notEmpty()
+  utils.l.i("Update user group request" + JSON.stringify(req.body))
+
   if(!req.body.id || !req.body.clanId) {
-    routeUtils.handleAPIError(req, res, {error:"Id and ClanId are rquired fields"}, {error:"Id and ClanId are rquired fields"})
+    var err = {
+      error: "Id and ClanId are required fields"
+    }
+    routeUtils.handleAPIError(req, res, err, err)
   }else {
     updateUserGroup(req.body, function (err, user) {
+      if (err) {
+        routeUtils.handleAPIError(req, res, err, err)
+      } else {
+        routeUtils.handleAPISuccess(req, res, user)
+      }
+    })
+  }
+}
+
+function updatePassword(req, res) {
+  utils.l.i("Update user password request" + JSON.stringify(req.body))
+
+
+  if(!req.body.id || !req.body.oldPassWord || !req.body.newPassWord) {
+    var err = {
+      error: "id, old password and new password are required fields"
+    }
+    routeUtils.handleAPIError(req, res, err, err)
+  } else {
+
+    try {
+      req.assert('newPassWord').notEmpty().isAlphaNumeric()
+    } catch(ex) {
+      var err = {
+        error: "password must be between 1 and 9 characters and must be alphanumeric"
+      }
+      return routeUtils.handleAPIError(req, res, err, err)
+    }
+
+    updateUserPassword(req.body, function (err, user) {
       if (err) {
         routeUtils.handleAPIError(req, res, err, err)
       } else {
@@ -67,12 +102,33 @@ function listUsers(callback) {
 }
 
 function updateUser(data, callback) {
-  models.user.updateUser(data, false,callback)
+  models.user.updateUser(data, false, callback)
 }
 
 //TODO: GroupID is set in clanID field. Need to change it later.
 function updateUserGroup(data, callback) {
-  models.user.updateUser({id:data.id,clanId:data.clanId}, true,callback)
+  models.user.updateUser({id: data.id, clanId: data.clanId}, true, callback)
+}
+
+function updateUserPassword(data, callback) {
+  utils.async.waterfall([
+    function(callback) {
+      getUserById(data, callback)
+    },
+    function(user, callback) {
+
+      if (!passwordHash.verify(data.oldPassWord, user.passWord)) {
+        return callback({error: "old password entered does not match the password in our records"}, null)
+      }
+
+      if (data.oldPassWord == data.newPassWord) {
+        return callback({error: "new password has to be different from the old password"}, null)
+      }
+
+      data.passWord = data.newPassWord
+      updateUser(data, callback)
+    }
+  ], callback)
 }
 
 routeUtils.rGet(router, '/self', 'GetSelfUser', getSelfUser)
@@ -80,4 +136,5 @@ routeUtils.rGet(router, '/list', 'list', list)
 routeUtils.rPost(router, '/listById', 'listById', listById)
 routeUtils.rPost(router, '/update', 'update', update)
 routeUtils.rPost(router, '/updateGroup', 'updateGroup', updateGroup)
+routeUtils.rPost(router, '/updatePassword', 'updatePassword', updatePassword)
 module.exports = router
