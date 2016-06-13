58840686fce9ca06e09e07f52cf69e274764beba
Suraj Abichandani <suraj.abichandani@verizon.com>
2016-06-13 14:59:14 -0700

    Sending push notification to those users who are logged in

--- app/models/schema/userSchema.js
+++ app/models/schema/userSchema.js
@@ -14,6 +14,7 @@ var UserSchema = new Schema({
   consoles: [{consoleType:consoleTypeEnum,consoleId:{type:String},verifyStatus:acctVerifyEnum,verifyToken:{type:String}}],
   clanId: { type: String, default: "clan_id_not_set"},
   imageUrl: String,
+  isLoggedIn: {type: Boolean, default: true},
   uDate: Date,
   signupDate: Date,
   flags: Mixed,
