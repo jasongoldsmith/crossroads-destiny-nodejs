92d43338665412b4224102eb5b7171271bb231df
Suraj Abichandani <suraj.abichandani@verizon.com>
2016-06-22 14:23:41 -0700

    Showing the notification status in list groups API

--- app/routes/v1/a/account.js
+++ app/routes/v1/a/account.js
@@ -7,53 +7,70 @@ var routeUtils = require('../../routeUtils')
 var models = require('../../../models')
 var service = require('../../../service/index')
 
-function listMyGroups(req,res){
+function listMyGroups(req, res) {
   listGroups(req.user, function(err, groups) {
     if (err) {
-      routeUtils.handleAPIError(req, res, err, err,{utm_dnt:"listMyGroups"})
+      routeUtils.handleAPIError(req, res, err, err, {utm_dnt: "listMyGroups"})
     } else {
       groupsResponse = groups || [{}]
-      routeUtils.handleAPISuccess(req, res, groupsResponse,{utm_dnt:"listMyGroups"})
+      routeUtils.handleAPISuccess(req, res, groupsResponse, {utm_dnt: "listMyGroups"})
     }
   })
 }
 
-function listGroups(user,callback){
+function listGroups(user, callback) {
   var groupList = null
   var userObj = null
   utils.async.waterfall([
-      function (callback) {
-        models.user.getUserById({id:user._id},callback)
-      },
-      function(user,callback){
-        if(user) {
-          userObj = user
-          //TODO: set current page to 1 for now. Change it when we have paging for groups.
-          service.destinyInerface.listBungieGroupsJoined(user.bungieMemberShipId,userObj.consoles[0].consoleType, 1, callback)
-        }else return callback({error:"User doesnot exist/logged in."})
-      },function(groups,callback){
-        if(groups) {
-          groupList = groups
-          groupList.push(utils.constants.freelanceBungieGroup)
-          models.user.updateUser(mergeGroups(userObj,groupList),false,function(err,user){
-            if(user) service.eventService.listEventCountByGroups(utils._.map(groupList, 'groupId'),userObj.consoles[0].consoleType,callback)
-            else return callback(err,null)
-          })
-        }else return callback(null, null)
-      },function(eventCounts, callback){
-        mergeEventStatsWithGroups(eventCounts,groupList, callback)
-      },function(groupEventStatsList,callback){
-        groupList=groupEventStatsList
-        service.authService.listMemberCountByClan(utils._.map(groupList, 'groupId'),userObj.consoles[0].consoleType,callback)
-      },function(memberCounts,callback){
-        mergeMemberStatsWithGroups(memberCounts,groupList,callback)
+    function (callback) {
+      models.user.getUserById({id: user._id}, callback)
+    },
+    function(user,callback) {
+      if(user) {
+        userObj = user
+        //TODO: set current page to 1 for now. Change it when we have paging for groups.
+        service.destinyInerface.listBungieGroupsJoined(user.bungieMemberShipId,
+          userObj.consoles[0].consoleType, 1, callback)
+      } else {
+        return callback({error: "User doesnot exist/logged in."})
       }
-    ],
-  callback
-  )
+    },
+    function(groups, callback) {
+      if(groups) {
+        groupList = groups
+        groupList.push(utils.constants.freelanceBungieGroup)
+        models.user.updateUser(mergeGroups(userObj, groupList), false, function(err, user) {
+          if (user) {
+            // Adding muteNotification status to user group list
+            utils._.map(groupList, function(group) {
+              var userGroup = utils._.find(user.groups, {"groupId": group.groupId})
+              group.muteNotification = userGroup.muteNotification
+            })
+            service.eventService.listEventCountByGroups(utils._.map(groupList, 'groupId'),
+              userObj.consoles[0].consoleType, callback)
+          } else {
+            return callback(err,null)
+          }
+        })
+      } else {
+        return callback(null, null)
+      }
+    },
+    function(eventCounts, callback) {
+      mergeEventStatsWithGroups(eventCounts,groupList, callback)
+    },
+    function(groupEventStatsList,callback) {
+      groupList = groupEventStatsList
+      service.authService.listMemberCountByClan(utils._.map(groupList, 'groupId'),
+        userObj.consoles[0].consoleType, callback)
+    },
+    function(memberCounts, callback) {
+      mergeMemberStatsWithGroups(memberCounts, groupList, callback)
+    }
+  ], callback)
 }
 
-function resendBungieMessage(req,res){
+function resendBungieMessage(req, res) {
   handleResendBungieMessage(req.user, function(err, user) {
     if (err) {
       routeUtils.handleAPIError(req, res, err, err)
@@ -62,43 +79,49 @@ function resendBungieMessage(req,res){
     }
   })
 }
+
 function handleResendBungieMessage(userData,callback){
-  utils.async.waterfall(
-    [
-      function(callback) {
-        //TBD: membershiptype is hardcoded to PSN for now. When we introduce multiple channels change this to take it from userdata
-        // or send notification to both xbox and psn depending on the ID availability
-        if(utils.config.enableBungieIntegration) {
-          service.destinyInerface.sendBungieMessage(userData.bungieMemberShipId,utils._.get(utils.constants.consoleGenericsId, userData.consoles[0].consoleType), utils.constants.bungieMessageTypes.accountVerification, function (error, messageResponse) {
-            utils.l.d('handleResendBungieMessage::messageResponse',messageResponse)
-            utils.l.d('handleResendBungieMessage::signupUser::sendBungieMessage::error',error)
+  utils.async.waterfall([
+    function(callback) {
+      //TBD: membershiptype is hardcoded to PSN for now. When we introduce multiple channels change this to take it from userdata
+      // or send notification to both xbox and psn depending on the ID availability
+      if(utils.config.enableBungieIntegration) {
+        service.destinyInerface.sendBungieMessage(userData.bungieMemberShipId,
+          utils._.get(utils.constants.consoleGenericsId, userData.consoles[0].consoleType),
+          utils.constants.bungieMessageTypes.accountVerification, function (error, messageResponse) {
+
+            utils.l.d('handleResendBungieMessage::messageResponse', messageResponse)
+            utils.l.d('handleResendBungieMessage::signupUser::sendBungieMessage::error', error)
             if (messageResponse) {
               utils.l.d("messageResponse::token===" + messageResponse.token)
               userData.consoles[0].verifyStatus = "INITIATED"
               userData.consoles[0].verifyToken = messageResponse.token
               var newUserObj = {
-                id:userData._id,
-                consoles:userData.consoles
+                id: userData._id,
+                consoles: userData.consoles
               }
-              callback(null, newUserObj)
+              return callback(null, newUserObj)
             } else {
               return callback(error, null)
             }
           })
-        }else {
+        } else {
           console.log("Destiny validation disabled")
-          callback(null, null)
+          return callback(null, null)
         }
-      },function (newUser, callback) {
-        if(newUser) models.user.updateUser(newUser, false, callback)  // don't send message
-        else callback(null, userData)
+      },
+    function (newUser, callback) {
+      if(newUser) {
+        // don't send message
+        models.user.updateUser(newUser, false, callback)
+      } else {
+        callback(null, userData)
       }
-    ],
-    callback
-  )
+    }
+  ], callback)
 }
 
-function searchGroupReq(req,res){
+function searchGroupReq(req, res) {
   searchGroup(req.user, req.param('groupId'),function(err, groups) {
     if (err) {
       routeUtils.handleAPIError(req, res, err, err)
@@ -108,78 +131,101 @@ function searchGroupReq(req,res){
   })
 }
 
-function searchGroup(user,groupId,callback){
+function searchGroup(user, groupId, callback){
   var userObj = null
   var groupList = null
   utils.async.waterfall([
-      function (callback) {
-        models.user.getUserById({id:user._id},callback)
-      },
-      function(user,callback){
-        if(user) {
-          userObj = user
-          //TODO: set current page to 1 for now. Change it when we have paging for groups.
-          service.destinyInerface.listBungieGroupsJoined(user.bungieMemberShipId, userObj.consoles[0].consoleType,1, callback)
-        }else return callback({error:"User doesnot exist/logged in."})
-      },function(groups,callback){
-        if(groups) {
-          groupList = groups
-          service.eventService.listEventCountByGroups(utils._.map(groups, 'groupId'),userObj.consoles[0].consoleType, callback)
-        }else return callback(null, null)
-      },function(eventCounts, callback){
-        mergeEventStatsWithGroups(eventCounts,groupList, callback)
+    function (callback) {
+      models.user.getUserById({id:user._id},callback)
+    },
+    function(user,callback){
+      if(user) {
+        userObj = user
+        //TODO: set current page to 1 for now. Change it when we have paging for groups.
+        service.destinyInerface.listBungieGroupsJoined(user.bungieMemberShipId,
+          userObj.consoles[0].consoleType,1, callback)
+      } else {
+        return callback({error: "User doesnot exist/logged in."})
+      }
+    },
+    function(groups,callback){
+      if(groups) {
+        groupList = groups
+        service.eventService.listEventCountByGroups(utils._.map(groups, 'groupId'),
+          userObj.consoles[0].consoleType, callback)
+      } else {
+        return callback(null, null)
+      }
+    },
+    function(eventCounts, callback) {
+        mergeEventStatsWithGroups(eventCounts, groupList, callback)
       }
     ],
-    function(err, bungieGroups){
-      if(err) return callback(err, null)
-      else{
-        utils.l.d("Groups::"+JSON.stringify(bungieGroups))
-        return callback(null,utils._.head(utils._.filter(bungieGroups,{groupId:groupId})))
+    function(err, bungieGroups) {
+      if(err) {
+        return callback(err, null)
+      } else {
+        utils.l.d("Groups::" + JSON.stringify(bungieGroups))
+        return callback(null, utils._.head(utils._.filter(bungieGroups, {groupId: groupId})))
       }
     }
   )
 }
 
-function mergeEventStatsWithGroups(eventCountList,groupList, callback){
+function mergeEventStatsWithGroups(eventCountList, groupList, callback){
   var groupUpdatedList = null
-  if(eventCountList){
-    //groupList = {groupList:groupList,eventStats:eventCounts}
-    groupUpdatedList = utils._.map(JSON.parse(JSON.stringify(groupList)),function(group){
-      var eventCount = utils._.find(eventCountList,{"_id":group.groupId})
-      if(eventCount) group.eventCount=eventCount.count
+  if(eventCountList) {
+    groupUpdatedList = utils._.map(JSON.parse(JSON.stringify(groupList)), function(group) {
+      var eventCount = utils._.find(eventCountList, {"_id": group.groupId})
+      if(eventCount) {
+        group.eventCount = eventCount.count
+      }
       return group
     })
-  }else groupUpdatedList = groupList
-
+  } else {
+    groupUpdatedList = groupList
+  }
   return callback(null, groupUpdatedList)
 }
 
-function mergeMemberStatsWithGroups(memberCounts,groupList, callback){
+function mergeMemberStatsWithGroups(memberCounts, groupList, callback) {
   var groupUpdatedList = null
-  if(memberCounts){
-    //groupList = {groupList:groupList,eventStats:eventCounts}
-    groupUpdatedList = utils._.map(JSON.parse(JSON.stringify(groupList)),function(group){
-      var userCount = utils._.find(memberCounts,{"_id":group.groupId})
-      if(userCount) group.memberCount=userCount.count
+  if(memberCounts) {
+    groupUpdatedList = utils._.map(JSON.parse(JSON.stringify(groupList)), function(group) {
+      var userCount = utils._.find(memberCounts, {"_id": group.groupId})
+      if(userCount) {
+        group.memberCount = userCount.count
+      }
       return group
     })
-  }else groupUpdatedList = groupList
-
+  } else {
+    groupUpdatedList = groupList
+  }
   return callback(null, groupUpdatedList)
 }
 
 function mergeGroups(user,bungieGroups){
   var bungieGroupIds = utils._.map(bungieGroups, 'groupId')
-  var updatedGroups = utils._.map(bungieGroupIds,function(bungieId){
-    var userGroup = utils._.find(user.groups,{groupId:bungieId})
-    if(!userGroup) return {groupId:bungieId,muteNotification:false}
-    else return userGroup
+  var updatedGroups = utils._.map(bungieGroupIds,function(bungieId) {
+    var userGroup = utils._.find(user.groups, {groupId: bungieId})
+    if(!userGroup) {
+      return {
+        groupId: bungieId,
+        muteNotification: false
+      }
+    } else {
+      return userGroup
+    }
   })
 
-  return {id:user._id,groups:updatedGroups}
+  return {
+    id: user._id,
+    groups: updatedGroups
+  }
 }
+
 /** Routes */
-routeUtils.rGet(router, '/group/list', 'listMyGroups', listMyGroups,{utm_dnt:"listMyGroups"})
+routeUtils.rGet(router, '/group/list', 'listMyGroups', listMyGroups, {utm_dnt:"listMyGroups"})
 routeUtils.rGet(router, '/group/search/:groupId', 'searchGroupById', searchGroupReq)
 routeUtils.rGet(router, '/group/resendBungieMessage', 'resendBungieMessage', resendBungieMessage)
 module.exports = router
