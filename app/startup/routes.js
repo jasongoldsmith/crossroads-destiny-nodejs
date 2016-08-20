57beae7a25ebfc1047056572bd443079a8d00748
Sreeharsha Dasa <setty.sreeharshadasaanjayaiah@one.verizon.com>
2016-08-19 20:16:48 -0700

    Include /api/v1/a/mixpanel/track path for backward compatibility

--- app/startup/routes.js
+++ app/startup/routes.js
@@ -53,7 +53,8 @@ module.exports = function (app, passport) {
   app.use('/api/v1/a/messages', require('../routes/v1/a/messages'))
   app.use('/api/v1/a/report', require('../routes/v1/a/report'))
   app.use('/api/v1/a/account', require('../routes/v1/a/account'))
-  app.use('/api/v1/mixpanel', require('../routes/v1/a/mixPanelDataTracking'))
+  app.use('/api/v2/mixpanel', require('../routes/v1/a/mixPanelDataTracking'))
+  app.use('/api/v1/a/mixpanel', require('../routes/v1/a/mixPanelDataTracking'))
 
   /// catch 404 and forward to error handler
   app.use(function(req, res, next) {
