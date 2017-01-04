15334872044a82a3fc5fdbc68a36e19cf2422e04
Suraj Abichandani <suraj.abichandani@verizon.com>
2017-01-04 12:08:55 -0800

    1. Updating aysnc version; 2. removing unnecessary code from test_events.js

--- app/tests/integration/mocha/default/test_event.js
+++ app/tests/integration/mocha/default/test_event.js
@@ -8,86 +8,74 @@ var service = require('../../../../service')
 describe("Successful event test cases: ", function() {
 
   this.timeout(30000)
-  describe("test create event: ", function() {
-    var loginData = {}
-    loginData.consoleType = "PS4"
+  var firstUserObj = null
+  var secondUserObj = null
+  var eventObj = null
+  it("create a new event: ", function(done) {
 
-    before(function(done) {
-      loginDataProvider.loginUser("sreeharshadasa","PS4",null,function(err,userObj){
-        user = userObj
-        done()
-      })
-    })
-
-    it("create a new event: ", function(done) {
-      var eventObj = null
-      utils.async.waterfall([
-        // login first user
-        function (callback) {
-          loginDataProvider.loginUser("sreeharshadasa","PS4", null, callback)
-        },
-        // create event
-        function (user, callback) {
-          var data = {
-            "eType": "56df5421dac7e703003a0f5c",
-            "minPlayers": 1,
-            "maxPlayers": 6
-          }
-          service.eventService.createEvent(user, data, function(err, event) {
-            if(err) {
-              return callback(err, event)
-            }
-            eventObj = event
-            validateSuccessfulEventCreate(event)
-            return callback(null, event)
-          })
-        },
-        // login second user
-        function (event, callback) {
-          loginDataProvider.loginUser("CrossroadsTina","PS4", null, callback)
-        },
-        // make second user join the event
-        function (user, callback) {
-          var data = {
-            eId: eventObj._id
-          }
-          service.eventService.joinEvent(user, data, function(err, event) {
-            if(err) {
-              return callback(err, event)
-            }
-            validateSuccessfulEventJoin(event, user)
-            return callback(null, user)
-          })
-        },
-        // make the user leave the event
-        function (user, callback) {
-          var data = {
-            eId: eventObj._id
-          }
-          service.eventService.leaveEvent(user, data, function(err, event) {
-            if(err) {
-              return callback(err, event)
-            }
-            validateSuccessfulEventLeave(event, user)
-            return callback(null, user)
-          })
+    utils.async.waterfall([
+      function loginFirstUser(callback) {
+        loginDataProvider.loginUser("sreeharshadasa", "PS4", null, callback)
+      },
+      function createEvent(user, callback) {
+        firstUserObj = user
+        var data = {
+          "eType": "56df5421dac7e703003a0f5c",
+          "minPlayers": 1,
+          "maxPlayers": 6
         }
-      ],
+        service.eventService.createEvent(user, data, function(err, event) {
+          if(err) {
+            return callback(err, event)
+          }
+          eventObj = event
+          validateSuccessfulEventCreate(event)
+          return callback(null, event)
+        })
+      },
+      function loginSecondUser(event, callback) {
+        loginDataProvider.loginUser("CrossroadsTina", "PS4", null, callback)
+      },
+      function secondUserJoinsEvent(user, callback) {
+        secondUserObj = user
+        var data = {
+          eId: eventObj._id
+        }
+        service.eventService.joinEvent(user, data, function(err, event) {
+          if(err) {
+            return callback(err, event)
+          }
+          validateSuccessfulEventJoin(event, user)
+          return callback(null, user)
+        })
+      },
+      function secondUserLeavesEvent(user, callback) {
+        var data = {
+          eId: eventObj._id
+        }
+        service.eventService.leaveEvent(user, data, function(err, event) {
+          if(err) {
+            return callback(err, event)
+          }
+          validateSuccessfulEventLeave(event, user)
+          return callback(null, user)
+        })
+      }
+    ],
       function (err, result) {
         if(!err) {
           done()
         }
       })
-    })
   })
 
   after(function() {
-    utils.l.d("Removing user",user)
-    models.user.model.remove({_id:user._id},function(err,data){})
-    models.userGroup.model.remove({"user" : user._id},function(err,data){})
-    models.eventInvitation.model.remove({"inviter" : user._id},function(err,data){})
-    models.eventInvitation.model.remove({"invitee" : user._id},function(err,data){})
-    models.event.model.remove({"players" : user._id},function(err,data){})
+    utils.l.d("Removing user", firstUserObj)
+    models.user.model.remove({_id: {$in: [firstUserObj._id, secondUserObj. _id]}}, function(err, data) {})
+    models.userGroup.model.remove({"user": {$in: [firstUserObj._id, secondUserObj. _id]}}, function(err, data) {})
+    models.eventInvitation.model.remove({"inviter": {$in: [firstUserObj._id, secondUserObj. _id]}}, function(err, data) {})
+    models.eventInvitation.model.remove({"invitee": {$in: [firstUserObj._id, secondUserObj. _id]}}, function(err, data) {})
+    models.event.model.remove({"players": {$in: [firstUserObj._id, secondUserObj. _id]}}, function(err, data) {})
   })
 })
 
